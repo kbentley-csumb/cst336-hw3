3c2068439c72877726abfef53a9302df5300c5ab
Kevin Bentley <kbentley@csumb.edu>
2019-07-07 23:42:15 +0000

    initial files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # cst336-hw3
+
+Using 
+http://deckofcardsapi.com/
+
+Button to create a deck, and a button to draw a card until empty.
+
+# Page Layout
+- bootstrap
+
+Header
+Button to create a new deck (shuffled)
+Button to reshuffle
+Display # cards left
+Text input, how many cards to draw (validate between 0 and the number remaining)
+Row for each 3 cards
+Hover for the high and low cards?
+
+- Footer
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Homework 3</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link  href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    </head>
+    <body >
+    <div id="container">
+    <main>
+    <h1>Heading</h1>
+This is the main content.
+
+    </main>
+
+    <footer>
+        <br/>
+        <br/>
+            <h3>CST336 Internet Programming.</h3> <br/>
+            <details>
+            <summary>API Information</summary>
+            &nbsp;&nbsp;&nbsp;&nbsp;Powered by http://deckofcardsapi.com/
+            </details>
+            2019&copy; Kevin Bentley<br/>
+            <strong>Disclaimer</strong> This information is only for academic purposes.
+                <br/>
+                <br/>
+                <img src="img/otter-small.jpg" alt="Otter Logo" />
+        </footer>
+
+    <script src="js/cards.js"></script>
+    </div> <!--container-->
+    </body>
+</html>
